2300f028143aff455b2b52b57f6e0fdc242a45e3
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-20 10:22:24 +0300

    start

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const creatLi = galleryItems
+  .map((galleryItem) =>`<a class="gallery__link" href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      data-source="${galleryItem.original}"
+      alt="${galleryItem.description}"
+    />
+  </a>`)
+  .join("");
+const gallery = document.querySelector(".gallery");
+gallery.classList.add('gallery__item')
+gallery.insertAdjacentHTML("afterbegin", creatLi); 
 
-console.log(galleryItems);
+function clic (e) {
+  console.log(e.target)
+}
+gallery.addEventListener('click', clic);
+
+gallery.addEventListener("click", openModal);
+
+function openModal (el) {
+  el.preventDefault ()
+  const instance = basicLightbox.create(`
+  <img src=${el.target.src} width="800" height="600">
+`)
+onShow: () => window.addEventListener ()
+instance.show()
+function esc (e) {
+  if (e.code === 'Escape')
+  {instance.close()}
+}
+}
