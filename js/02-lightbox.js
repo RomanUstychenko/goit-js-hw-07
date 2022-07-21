24258f896e67e07a9351a40592db020e21a4107f
RomanUstychenko <roman.ustychenko@gmail.com>
2022-07-21 10:23:29 +0300

    start 02-lightbox

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const creatList = galleryItems
+  .map((galleryItem) =>
+  `<a class="gallery__item" href="${galleryItem.original}">
+  <img 
+  class="gallery__image" 
+  src="${galleryItem.preview}" 
+  alt="${galleryItem.description}" />
+</a>`)
+  .join("");
+  const gallery = document.querySelector(".gallery");
+gallery.classList.add('gallery__item')
+gallery.insertAdjacentHTML("afterbegin", creatList); 
 
-console.log(galleryItems);
+
